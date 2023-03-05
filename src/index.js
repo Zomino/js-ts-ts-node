bcfb071229669d1c118cf40d2a520027c24c8482
Zou Minowa <zou.minowa@shoppermediagroup.com>
2023-03-05 13:31:11 +0000

    add nodemon

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
+import express from 'express';
+
 import hello from './hello';
 
-console.log(hello());
+const app = express();
+
+app.get('/', (_, res) => res.send(hello()));
+
+app.listen(3000, () => console.log('Server up: http://localhost:3000'));
